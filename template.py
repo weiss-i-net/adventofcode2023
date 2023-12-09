6a6b62a790095569db2f1e85daf7a44681eb0e31
Jannik Hiller <jannik.hiller@rwth-aachen.de>
2023-12-09 06:12:29 -0800

    change debug print function

--- template.py
+++ template.py
@@ -1,14 +1,16 @@
 from collections import Counter, defaultdict, deque
-import itertools as it
 import more_itertools as mit
 import functools as func
+import itertools as it
 import operator as op
 import networkx as nx
+import numpy as np
+import sympy
 import math
 import re
 
-def p(v):
-    print(v)
+def p(v, desc=None):
+    print(f"{desc}: {v}" if desc else v)
     return v
 
 with open("input") as f:
